d93e131b5e861d93f807a91dd364019e67c4cc28
Narath Carlile <narathc@gmail.com>
2020-08-04 14:50:41 -0400

    Limited sharing resource controller with testing

---
+++ lib/resource_controllers/sharing_with_limits.rb
@@ -0,0 +1,93 @@
+require_relative File.join("../", "resource_controller")
+
+class ResourceControllerSharingWithLimits < ResourceController
+  attr_reader :limit
+
+  # beds is a hash by name of the population
+  # limit = the percentage of beds that are kept for local use only
+  def initialize(name = self.class.name, limit: 0)
+    @name = name
+    @beds = {}
+    @max_beds = {}
+    @limit = limit
+  end
+
+  def register(population)
+    @beds[population.name] = {
+      available: default_beds_per_population(population),
+      max: default_beds_per_population(population), 
+      used_locally: 0 }
+  end
+
+  # beds available for a population
+  # is the number of beds available locally
+  # and the number of beds available non locally
+  def beds_available(population)
+    beds_available_locally(population) + beds_available_shared(population)
+  end
+
+  def beds_available_locally(population)
+    @beds[population.name][:available]
+  end
+
+  def beds_available_shared(population)
+    result = 0
+    @beds.each do |key, value|
+      next if key == population.name
+      result += beds_available_for_sharing(key)
+    end
+    result
+  end
+
+  # take beds locally if available
+  # if not available locally, take as many beds as possible
+  # then take equally from all those available in the rest
+  # it returns an array with the name of each of the populations where this was taken from
+  def take_beds(population, beds)
+    raise "Too many beds taken" if beds_available(population)<beds
+    result = []
+
+    local = beds_available_locally(population)
+    if (local>=beds)
+      @beds[population.name][:available] -= beds
+      @beds[population.name][:used_locally] += beds
+      result += (1..beds).collect {|c| population.name } 
+    else
+      if local>0
+        @beds[population.name][:available] -= local
+        @beds[population.name][:used_locally] += local
+        result += (1..local).collect { |c| population.name }
+      end
+      
+      beds_needed = beds - local
+      while beds_needed>0
+        @beds.each do |k, v|
+          break if beds_needed<=0
+          next if k == population.name
+          if beds_available_for_sharing(k)>0
+            v[:available] -= 1
+            beds_needed -= 1
+            result += [k]
+          end
+        end
+      end
+    end
+    result
+  end
+
+  # when returning beds, we return them locally to used_locally
+  # then we return them equally to the rest to the max - used locally
+  def return_beds(population, beds)
+    @beds = @beds + beds
+    raise "Too many beds returned" if @beds>@max_beds
+    beds
+  end
+
+  def beds_available_for_sharing(name)
+    limit_beds = @beds[name][:max] * @limit / 100
+    [@beds[ name][:available] - limit_beds, 0].max
+  end
+
+end
+
+
---
+++ test/lib/resources_controllers/resource_controller_sharing_with_limits_test.rb
@@ -0,0 +1,115 @@
+require 'minitest/autorun'
+
+require_relative "../../../lib/resource_controllers/sharing_with_limits"
+require_relative "../../../lib/population"
+
+describe ResourceControllerSharingWithLimits do
+
+  it "counts available properly with no limits" do
+    rc = ResourceControllerSharingWithLimits.new
+    p1 = Population.new("s1", rc)
+    p2 = Population.new("s2", rc)
+
+    b1 = rc.default_beds_per_population(p1)
+    b2 = rc.default_beds_per_population(p2)
+
+    # with no limits on sharing - all beds should be available
+    assert_equal b1+b2, rc.beds_available(p1)
+  end
+
+  it "counts available properly with 100% limits" do
+    rc = ResourceControllerSharingWithLimits.new("100%", limit:100)
+    p1 = Population.new("s1", rc)
+    p2 = Population.new("s2", rc)
+
+    b1 = rc.default_beds_per_population(p1)
+    b2 = rc.default_beds_per_population(p2)
+
+    # with no limits on sharing - all beds should be available
+    assert_equal b1, rc.beds_available(p1)
+  end
+
+  it "counts available properly with 50% limits" do
+    rc = ResourceControllerSharingWithLimits.new(limit:50)
+    p1 = Population.new("s1", rc)
+    p2 = Population.new("s2", rc)
+
+    b1 = rc.default_beds_per_population(p1)
+    b2 = rc.default_beds_per_population(p2)
+
+    # with no limits on sharing - all beds should be available
+    assert_equal b1 + (b2 * 0.5), rc.beds_available(p1)
+  end
+
+  it "takes beds fairly when all local beds are used" do
+    rc = ResourceControllerSharingWithLimits.new(limit:50)
+    p1 = Population.new("s1", rc)
+    p2 = Population.new("s2", rc)
+    p3 = Population.new("s3", rc)
+
+    b1 = rc.default_beds_per_population(p1)
+    b2 = rc.default_beds_per_population(p2)
+    b3 = rc.default_beds_per_population(p3)
+
+    assert_equal b1, rc.take_beds(p1, b1).count
+    assert_equal 2, rc.take_beds(p1, 2).count
+    assert_equal b2-1, rc.beds_available_locally(p2)
+    assert_equal b3-1, rc.beds_available_locally(p3)
+  end
+
+  it "takes beds locally when available" do
+    rc = ResourceControllerSharingWithLimits.new
+    p1 = Population.new("s1", rc)
+    p2 = Population.new("s2", rc)
+
+    b1 = rc.default_beds_per_population(p1)
+    b2 = rc.default_beds_per_population(p2)
+
+    assert_equal ["s1"], rc.take_beds(p1, 1)
+  end
+
+  it "takes beds remotely when available" do
+    rc = ResourceControllerSharingWithLimits.new
+    p1 = Population.new("s1", rc)
+    p2 = Population.new("s2", rc)
+
+    b1 = rc.default_beds_per_population(p1)
+    b2 = rc.default_beds_per_population(p2)
+
+    rc.take_beds(p1,b1)
+    assert_equal ["s2"], rc.take_beds(p1, 1)
+  end
+
+  it "takes beds fairly remotely when available" do
+    rc = ResourceControllerSharingWithLimits.new
+    p1 = Population.new("s1", rc)
+    p2 = Population.new("s2", rc)
+    p3 = Population.new("s3", rc)
+
+    b1 = rc.default_beds_per_population(p1)
+
+    rc.take_beds(p1,b1)
+    assert_equal ["s2", "s3"], rc.take_beds(p1, 2)
+  end
+
+  it "returns beds to the location taken from" do
+    rc = ResourceControllerSharingWithLimits.new
+    p1 = Population.new("s1", rc)
+    p2 = Population.new("s2", rc)
+    p3 = Population.new("s3", rc)
+
+    b1 = rc.default_beds_per_population(p1)
+
+    taken1 = rc.take_beds(p1,b1)
+    taken2 = rc.take_beds(p1,2)
+    assert_equal b1-1, beds_available_locally(p2)
+    assert_equal b1-1, beds_available_locally(p3)
+    assert_equal 0, beds_available_locally(p1)
+
+    return_beds(p1, taken2)
+    assert_equal b1, beds_available_locally(p2)
+    assert_equal b1, beds_available_locally(p3)
+    assert_equal 0, beds_available_locally(p1)
+  end
+
+end
